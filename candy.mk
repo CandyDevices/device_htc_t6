e4e3fcf16996c30ee683fd91239fe2703db5680d
gimme <gimmeitorilltell@gmail.com>
2018-04-15 16:40:23 -0400

    Revert "t6: Remove enhanced NFC feature"
    This reverts commit c75a4b4a0fdb2052f6885f35e714a48d97a485ec.

--- candy.mk
+++ candy.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
+$(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
+
 $(call inherit-product, device/htc/t6/full_t6.mk)
 
 PRODUCT_NAME := candy_t6
